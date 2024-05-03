8094b1a26cdaf1f0d9dc0805f2b28db0fdbbfeef
Tess <tesshuangxj@yahoo.com>
2024-05-03 15:57:50 -0700

    Add icon pkg and update translate ui

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -1,16 +1,45 @@
-import { FormControl, FormLabel, Textarea, Text, Box } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Textarea,
+  Text,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
 
 export const TextInput = (props: { name?: string; isEditable?: boolean }) => {
   const { name, isEditable = false } = props;
+
   return (
     <div>
       <FormControl borderWidth="1px" borderRadius="lg" p={4} boxShadow="md">
-        <FormLabel fontSize="xl">{name}</FormLabel>
+        <FormLabel fontSize="xl" m={0}>
+          {name}
+        </FormLabel>
         {isEditable ? (
-          <Textarea placeholder="Enter your text" border="none" p={2} />
+          <Flex>
+            <Textarea
+              placeholder="Enter your text"
+              border="none"
+              py={2}
+              px={0}
+              flex="1"
+            />
+            <CloseIcon
+              mt={2}
+              boxSize={5}
+              focusable={true}
+              bgColor="gray.400"
+              color="gray.100"
+              borderRadius="50%"
+              p={1}
+              aria-label="reset text input"
+            />
+          </Flex>
         ) : (
-          <Box p={2} minHeight={20}>
-            <Text fontSize="md" color="gray.500" px={2}>
+          <Box minHeight={20}>
+            <Text fontSize="md" color="gray.500" py={2}>
               Translation
             </Text>
           </Box>
--- src/app/components/Translation.tsx
+++ src/app/components/Translation.tsx
@@ -1,10 +1,25 @@
+"use client";
+import { Box, AbsoluteCenter } from "@chakra-ui/react";
 import { TextInput } from "./TextInput";
+import { ArrowUpDownIcon } from "@chakra-ui/icons";
 
 export const Translation = () => {
   return (
-    <div>
+    <Box position="relative">
       <TextInput name="French" isEditable />
+      <AbsoluteCenter axis="both" zIndex={1}>
+        <ArrowUpDownIcon
+          boxSize={10}
+          focusable={true}
+          bgColor="gray.800"
+          color="gray.100"
+          borderRadius="50%"
+          p={2}
+          aria-label="switch the tranlsation"
+        />
+      </AbsoluteCenter>
+      <Box p={1} />
       <TextInput name="English" />
-    </div>
+    </Box>
   );
 };
