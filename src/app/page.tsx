9540c453647ca0dda8718d58b7c78b00ac11709f
Tess <tesshuangxj@yahoo.com>
2024-05-31 14:24:11 -0700

    Sort data from prisma by createAt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import { VStack } from "@chakra-ui/react";
 import { TranslateHome } from "./components/TranslateHome";
 
 export default async function Home() {
-  const allFavourites = await prisma.favorite.findMany();
+  const allFavourites = await prisma.favorite.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  console.log("allFavourites from prisma, ", allFavourites);
 
   return (
     <VStack as="main" align="stretch" p={4}>
