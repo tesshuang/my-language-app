3e0ba3f47b6d0a12982edbca2a353651ccc25aa9
Tess <tesshuangxj@yahoo.com>
2024-06-14 16:07:55 -0700

    [api]: Add api/favorite patch request to create new record and connect

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -16,5 +16,4 @@ export async function POST(request: Request) {
   })  
 
   return Response.json({data: category}, {status: 200})
- 
 }
--- src/app/api/favorite/[id]/route.ts
+++ src/app/api/favorite/[id]/route.ts
@@ -1,7 +1,7 @@
 import prisma from '../../../utils/prisma'
 
 // DELETE /api/favorite/:id
-// Required fields in body: id
+// delete the item in favorite table
 
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
   try{
@@ -9,6 +9,43 @@ export async function DELETE(request: Request, { params }: { params: { id: strin
 
    const favorite = await prisma.favorite.delete({
       where: { id: id },
+    })
+
+    return Response.json(favorite)
+
+  } catch(e: any) {
+    return new Response(`Delete favorite error: ${e.message}`, {
+      status: 400,
+    })
+  }
+}
+
+// PATCH /api/favorite/:id
+// Required fields in body: categoryIds
+// updates the resource with the request category payload 
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  try{
+    const id = params.id
+    const data  = await request.json()
+    const { categoryIds }: {categoryIds: number[]} = data
+
+    const createCategories = categoryIds.map(id => ({
+      assignedAt: new Date(),
+      category: {
+        connect: {
+          id: id,
+        },
+      },
+    }));
+
+   const favorite = await prisma.favorite.update({
+      where: { id: id },
+      data: {
+        categories: {
+          create: createCategories,
+        }
+      }
     })
 
     return Response.json(favorite)
